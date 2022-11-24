4f66ed86085f4594cbc052612fc8a11c07aa56a9
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-11-24 09:28:39 +0300

    Add files via upload

---
+++ Lab3_prog/src/Animated/Alice.java
@@ -0,0 +1,53 @@
+package Animated;
+
+import Inanimated.Dish;
+import java.util.Objects;
+public class Alice extends Human {
+
+    private String condition;
+    public Alice(String name, int appetite, boolean gender){
+        super(name, appetite, gender);
+    }
+
+    public String getCondition(){
+        return condition;
+    }
+
+    @Override
+    public String toString(){
+        return "Alice_" + getName();
+    }
+
+    @Override
+    public boolean equals(Object condition){
+        if (this.getCondition() == condition){
+            return true;
+        }
+        if (condition == null || getClass() != condition.getClass()){
+            return false;
+        }
+        this.condition = (String) condition;
+        return Objects.equals(condition, this.condition);
+    }
+
+    @Override
+    public int hashCode(){
+        return (this.condition == null) ? 0 : this.condition.hashCode();
+    }
+
+    public String Hunger(){
+        while(this.getAppetite() + Dish.getCountOfPies() == 50){
+            this.appetite += 1;
+            this.condition = "недостаточно голодна";
+        }
+        this.condition = " прямо слюнки потекли.";
+        return this.condition;
+    }
+
+    public String Dream(){
+        if(this.equals(" прямо слюнки потекли.") && this.condition.hashCode() == this.hashCode()){
+            return "\"Хорошо бы, суд уже кончился и позвали к столу!\" - подумала она.";
+        }
+        return null;
+    }
+}
---
+++ Lab3_prog/src/Animated/King.java
@@ -0,0 +1,7 @@
+package Animated;
+
+public class King extends Human {
+    public King(String name, int appetite, boolean gender){
+        super(name, appetite, gender);
+    }
+}
